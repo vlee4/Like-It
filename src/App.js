4321f1db28722e9b81a762a5232488a76bfb0942
Victoria <vicglee1234007@gmail.com>
2020-11-18 16:08:54 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -4,11 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>I Like It</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
