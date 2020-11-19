00055d628cd91c667b074799e35e510025964db0
Victoria <vicglee1234007@gmail.com>
2020-11-18 21:11:16 -0600

    Set up firebase hosting & realtime database syncing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import firebase from "firebase";
 
-function App() {
+
+class App extends React.Component {
+  //database.ref refers to root of db, child("key") creates a child key on that object called "key"
+  // const dbRef = firebase.database().ref().child()
+  constructor(){
+    super()
+    this.state = {
+      movieName: "Spirited Away"
+    }
+  }
+
+  componentDidMount(){
+    const dbRef = firebase.database().ref().child('movies');
+    const movieRef = dbRef.child("movieName");
+    movieRef.on("value", snap=>{
+      this.setState({
+        movieName: snap.val()
+      })
+    })
+  }
+
+render(){
   return (
     <div className="App">
       <h1>I Like It</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>{this.state.movieName}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +42,7 @@ function App() {
       </header>
     </div>
   );
+}
 }
 
 export default App;
