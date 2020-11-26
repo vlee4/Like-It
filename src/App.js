58b984df95bc20f561963f90fee1273216874b4a
Victoria <vicglee1234007@gmail.com>
2020-11-26 02:52:43 -0600

    Added Footer content

--- src/App.js
+++ src/App.js
@@ -12,6 +12,11 @@ render(){
       <div className="content">
         <Routes/>
       </div>
+      <footer>
+        <div>Last updated: November 2020</div>
+        <div>Created by vLEE</div>
+        <div><a href="https://github.com/vlee4/Like-It">Source Code</a></div>
+      </footer>
     </div>
   );
 }
