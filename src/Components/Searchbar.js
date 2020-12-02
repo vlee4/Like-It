81fc680ac6181c989abd9e69b47d904c5537d60b
Victoria <vicglee1234007@gmail.com>
2020-12-01 18:28:39 -0600

    Rename test

--- src/Components/Searchbar.js
+++ src/Components/Searchbar.js
@@ -1,6 +1,6 @@
 import React from "react";
-import {connect} from "react-redux"
-import {searchMovies} from "../Store/moviesReducer"
+import {connect} from "react-redux";
+import {searchMovies} from "../Store/moviesReducer";
 
 class SearchBar extends React.Component {
   constructor(){
